6a2bf587d1109d62e5968f9195814c63d71129c4
hellomrA <hellomr@inbox.ru>
2018-03-13 14:49:29 +0300

    commit-2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,15 @@
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException{
+        StringStorage storage = new StringStorage();
+        new ThreadEntity(1, storage);
+        new ThreadEntity(2, storage);
+
+        while (true) {
+            System.out.println(storage.get());
+            Thread.sleep(1000);
+        }
+
     }
 }
---
+++ src/main/java/ThreadEntity.java
@@ -0,0 +1,43 @@
+/**
+ * Created by Антон on 12.03.2018.
+ */
+public class ThreadEntity implements Runnable {
+
+    private static final int DELAY = 200;
+
+    private volatile static boolean isStopped = false;
+    private int num;
+    private StringStorage storage;
+
+    public ThreadEntity(int num, StringStorage storage) {
+        this.num = num;
+        this.storage = storage;
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        while (!isStopped) {
+            try {
+                Thread.sleep(DELAY);
+                synchronized (storage) {
+                    int count = 0;
+                    while (count < 10 && !isStopped) {
+                        storage.add(num);
+                        count++;
+                        Thread.sleep(DELAY);
+                    }
+                }
+                Thread.yield();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void setIsStopped(boolean value) {
+        isStopped = value;
+    }
+
+
+}
